dc59b52ad54399dc28ce64497def5b91cb33eecb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-26 15:53:19 +0300

    Added another flex container under the second banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -81,6 +81,9 @@ export default function Home() {
 				linkName='/search?purpose=for-sale'
 				imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
 			/>
+			<Flex flexWrap={'wrap'}>
+				{/* Fetch the properties and map over them */}
+			</Flex>
 		</Box>
 	)
 }
