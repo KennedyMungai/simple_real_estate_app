7735194cd3ec32e35cbaa5c4076bd5b83bce5bb8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-26 16:15:48 +0300

    Stored the base url inside a const

--- utils/fetchApi.ts
+++ utils/fetchApi.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
+const baseUrl = 'https://bayut.p.rapidapi.com'
 // const headers = {
 // 	'x-rapidapi-host': process.env.REACT_APP_RAPID_API_HOST,
 // 	'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY
