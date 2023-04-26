49eebd791da45d416256ff66f4bfc3488badae80
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-26 15:50:37 +0300

    Refactored the component markup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -56,30 +56,28 @@ const Banner = ({
 
 export default function Home() {
 	return (
-		<Flex justify={'center'} p={'1rem'}>
-			<VStack spacing={'1rem'}>
-				<Heading>Real Estate Bitches!!!</Heading>
-				<Banner
-					purpose={'Rent A Home'}
-					title1={'Rental Homes for '}
-					title2={'Everyone'}
-					desc1={'Explore apartments, villas, homes'}
-					desc2={'and more'}
-					buttonText='Explore Renting'
-					linkName='/search?purpose=for-rent'
-					imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
-				/>
-				<Banner
-					purpose={'Buy A Home'}
-					title1={'Find, Buy and Own your '}
-					title2={'Dream Home'}
-					desc1={'Explore apartments, villas, homes'}
-					desc2={'and more'}
-					buttonText='Explore Buying'
-					linkName='/search?purpose=for-sale'
-					imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
-				/>
-			</VStack>
-		</Flex>
+		<Box>
+			<Heading>Real Estate Bitches!!!</Heading>
+			<Banner
+				purpose={'Rent A Home'}
+				title1={'Rental Homes for '}
+				title2={'Everyone'}
+				desc1={'Explore apartments, villas, homes'}
+				desc2={'and more'}
+				buttonText='Explore Renting'
+				linkName='/search?purpose=for-rent'
+				imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
+			/>
+			<Banner
+				purpose={'Buy A Home'}
+				title1={'Find, Buy and Own your '}
+				title2={'Dream Home'}
+				desc1={'Explore apartments, villas, homes'}
+				desc2={'and more'}
+				buttonText='Explore Buying'
+				linkName='/search?purpose=for-sale'
+				imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
+			/>
+		</Box>
 	)
 }
