95c91a1234b26801a61c437539f78b1dc7bbc822
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-26 15:04:37 +0300

    Added a bunch of imports for the react file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,7 @@
+import { Box, Button, Flex, Text } from '@chakra-ui/react'
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function Home() {
 	return <p>Something in the way</p>
 }
