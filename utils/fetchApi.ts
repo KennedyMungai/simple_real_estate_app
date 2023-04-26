f38865338b9ec93a9267578a84c00b48a8ef6d69
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-26 16:13:26 +0300

    Set up the headers object

--- utils/fetchApi.ts
+++ utils/fetchApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-headers: {
-    'x-rapidapi-host': process.env.REACT_APP_RAPID_API_HOST
-    'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY
+const headers = {
+	'x-rapidapi-host': process.env.REACT_APP_RAPID_API_HOST,
+	'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY
 }
