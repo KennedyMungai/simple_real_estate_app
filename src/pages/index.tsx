e2bfd651a740c07c2278490deb677729e70b1f74
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-26 15:12:28 +0300

    Created the Banner react component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,12 @@ import { Box, Button, Flex, Heading, Text } from '@chakra-ui/react'
 import Image from 'next/image'
 import Link from 'next/link'
 
+const Banner = ({ purpose }: { purpose: string }) => (
+	<Flex flexWrap={'wrap'} justify={'center'} align={'center'} m={'10'}>
+		{purpose}
+	</Flex>
+)
+
 export default function Home() {
 	return (
 		<Flex justify={'center'}>
