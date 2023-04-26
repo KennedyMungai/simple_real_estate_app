83d321441ff9ab8a06884613e8305ac3923c4deb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-26 15:45:50 +0300

    Updated the markup react components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ const Banner = ({
 	desc2: string
 	linkName: string
 }) => (
-	<Flex flexWrap={'wrap'} justify={'center'} align={'center'} m={'10'}>
+	<Flex flexWrap={'wrap'} justifyContent={'center'} alignItems={'center'} m={'10'}>
 		<Image src={imageUrl} width={500} height={300} alt={'banner'} />
 		<Box p={5}>
 			<Text color={'gray.500'} fontSize='sm' fontWeight='medium'>
@@ -64,7 +64,16 @@ export default function Home() {
 					linkName='/search?purpose=for-rent'
 					imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
 				/>
-				{/* <Banner purpose={'For Rent'} /> */}
+				<Banner
+					purpose={'Buy A Home'}
+					title1={'Find, Buy and Own your '}
+					title2={'Dream Home'}
+					desc1={'Explore apartments, villas, homes'}
+					desc2={'and more'}
+					buttonText='Explore Buying'
+					linkName='/search?purpose=for-sale'
+					imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
+				 />
 			</VStack>
 		</Flex>
 	)
